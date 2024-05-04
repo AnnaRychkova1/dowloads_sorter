b4a3e2cdf0c6796352b0ef4f28616a5aa3f5e693
trubchaninov Andriy 1-8 <a.qwerty0444@gmail.com>
2024-05-04 13:10:31 +0300

    add sort

--- index.js
+++ index.js
@@ -5,19 +5,54 @@
 
 import { program } from "commander";
 import path from "path";
+import fs from "fs/promises";
+import fsSync from "fs";
 
 program.option("-dn, --dirname <string>");
-
 program.parse();
-
 const options = program.opts();
-console.log(options);
-// const limit = options.first ? 1 : undefined;
-// console.log(program.args[0].split(options.separator, limit));
+const dirDict = {
+  images: [".png", ".jpg", ".svg", ".gif", ".webp"],
+  media: [".mp3", ".mp4", ".mkv", ".mov", ".avi"],
+  documents: [".doc", ".docx", ".xlsx", ".pdf", ".pptx", ".txt"],
+  archives: [".zip", ".vinrar", ".rar"],
+  others: [],
+};
 
-function dirHander(dirpath) {
+async function dirHandler(dirpath) {
   const abspath = path.resolve(dirpath);
-  console.log(abspath);
+  const files = await fs.readdir(abspath);
+  for (const file of files) {
+    const fullPath = path.join(abspath, file);
+    fsSync.stat(fullPath, (err, stats) => {
+      if (stats.isDirectory()) {
+        dirHandler(fullPath);
+      } else {
+        fileHandler(fullPath);
+      }
+    });
+  }
 }
 
-dirHander(options.dirname);
+function fileHandler(filePath) {
+  const { ext, base } = path.parse(filePath);
+  const dirName = Object.entries(dirDict).find((el) => {
+    const [_, extArr] = el;
+    return extArr.includes(ext);
+  });
+  //console.log(dirName[0]);
+  const abspath = path.resolve(options.dirname);
+  fsSync.readdir(abspath, (err, files) => {
+    const isDirectExist = files.some((file) => file === dirName[0]);
+    console.log(isDirectExist);
+    const finishDir = path.join(abspath, dirName[0]);
+    if (!isDirectExist) {
+      fsSync.mkdir(finishDir, () => {
+        fsSync.rename(filePath, path.join(finishDir, base), () => {});
+      });
+    } else {
+      fsSync.rename(filePath, path.join(finishDir, base), () => {});
+    }
+  });
+}
+dirHandler(options.dirname);
